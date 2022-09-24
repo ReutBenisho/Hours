e21b72d1ca5fbaa8182eaac07082693918e8d681
Reut Benisho <xreutx197@gmail.com>
2022-09-24 23:00:32 +0300

    Refer login activity to register activity + the other way around

--- app/src/main/java/com/example/hours/LoginActivity.java
+++ app/src/main/java/com/example/hours/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.hours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView lblRegister = findViewById(R.id.lbl_create_account);
+        lblRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hours/RegisterActivity.java
+++ app/src/main/java/com/example/hours/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.hours;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        TextView lblAlreadyHaveAccount = findViewById(R.id.lbl_already_have_account);
+        lblAlreadyHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
