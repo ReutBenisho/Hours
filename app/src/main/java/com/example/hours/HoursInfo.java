e9976b7e9015a4bfa6b9d33009ef046e83b277ba
Reut Benisho <xreutx197@gmail.com>
2022-09-25 10:42:07 +0300

    Added main calculation activity, defaults, calculation class, and time class

---
+++ app/src/main/java/com/example/hours/HoursInfo.java
@@ -0,0 +1,12 @@
+package com.example.hours;
+
+public class HoursInfo {
+    public CalcTime mArrivalTime;
+    public CalcTime mHalfDay;
+    public CalcTime mFullDay;
+    public CalcTime mZeroHours;
+    public CalcTime m3AndHalfHours;
+    public CalcTime m6Hours;
+    public boolean tookLaunchBreak = false;
+    public boolean tookEveningBreak;
+}
