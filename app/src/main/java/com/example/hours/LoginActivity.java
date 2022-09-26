ac67ae4187fc1912a24986edf9a37d1e610daed9
Reut Benisho <xreutx197@gmail.com>
2022-09-27 02:33:42 +0300

    Support biometric authentication (fingerprint) when logging in

--- app/src/main/java/com/example/hours/LoginActivity.java
+++ app/src/main/java/com/example/hours/LoginActivity.java
@@ -1,14 +1,21 @@
 package com.example.hours;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import java.util.concurrent.Executor;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private Executor mExecutor;
+    private BiometricPrompt mBiometricPrompt;
+    private BiometricPrompt.PromptInfo mPromptInfo;
     private TextView mLblRegister;
     private TextView mLblSignInAsGuest;
 
@@ -32,5 +39,31 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(new Intent(LoginActivity.this, ExitTimeActivity.class));
             }
         });
+
+        mExecutor = ContextCompat.getMainExecutor(this);
+        mBiometricPrompt = new BiometricPrompt(LoginActivity.this, mExecutor, new BiometricPrompt.AuthenticationCallback() {
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString) {
+                super.onAuthenticationError(errorCode, errString);
+            }
+
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+                super.onAuthenticationSucceeded(result);
+                startActivity(new Intent(LoginActivity.this, ExitTimeActivity.class));
+            }
+
+            @Override
+            public void onAuthenticationFailed() {
+                super.onAuthenticationFailed();
+            }
+        });
+
+        mPromptInfo = new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Authentication")
+                .setSubtitle("Login using fingerprint")
+                .setNegativeButtonText("Enter password instead")
+                .build();
+        mBiometricPrompt.authenticate(mPromptInfo);
     }
 }
