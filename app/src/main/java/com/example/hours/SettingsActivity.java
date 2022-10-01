e2dcd579894bc1be4a4fc6e3fcf15b863ec3e55e
Reut Benisho <xreutx197@gmail.com>
2022-10-01 12:50:59 +0300

    Created general preferences

--- app/src/main/java/com/example/hours/SettingsActivity.java
+++ app/src/main/java/com/example/hours/SettingsActivity.java
@@ -1,16 +1,25 @@
 package com.example.hours;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class SettingsActivity extends AppCompatActivity implements
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
@@ -53,6 +62,19 @@ public class SettingsActivity extends AppCompatActivity implements
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        PreferenceManager.setDefaultValues(this, R.xml.header_preferences, false);
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        prefs.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+            @Override
+            public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
+                SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(SettingsActivity.this);
+                if(s == getString(R.string.pref_dark_mode)){
+                    Snackbar.make(findViewById(R.id.settings), "Pressed on dark mode", Snackbar.LENGTH_SHORT).show();
+                } else if(s == getString(R.string.pref_student_mode)){
+                    Snackbar.make(findViewById(R.id.settings), "Pressed on student mode", Snackbar.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -109,6 +131,33 @@ public class SettingsActivity extends AppCompatActivity implements
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.sync_preferences, rootKey);
+        }
+    }
+
+    public static class GeneralFragment extends PreferenceFragmentCompat {
+
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.general_preferences, rootKey);
+        }
+
+        @Override
+        public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            Preference myPref = (Preference) findPreference(getString(R.string.pref_reset_general_settings));
+            myPref.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+                    Snackbar.make(container.findViewById(R.id.settings), "clicked on reset", Snackbar.LENGTH_SHORT).show();
+                    ResetGeneralSettings();
+                    return false;
+                }
+            });
+            return super.onCreateView(inflater, container, savedInstanceState);
+        }
+
+        private void ResetGeneralSettings() {
+            //TODO: ADD LOGIC TO RESET ALL GENERAL SETTINGS
+
         }
     }
 }
--- app/src/main/java/com/example/hours/ui/calcDay/CalcDayFragment.java
+++ app/src/main/java/com/example/hours/ui/calcDay/CalcDayFragment.java
@@ -90,7 +90,6 @@ public class CalcDayFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         mViewModel = new ViewModelProvider(this).get(CalcDayViewModel.class);
-        // TODO: Use the ViewModel
     }
 
 
